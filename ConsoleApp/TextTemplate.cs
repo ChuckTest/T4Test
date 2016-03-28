6c344bbab6d5c8e12093c56041fe76739e14ec73
ChuckLu <244657538@qq.com>
2016-03-28 11:16:32 +0800

    给属性增加换行

--- ConsoleApp/TextTemplate.cs
+++ ConsoleApp/TextTemplate.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 public class Chuck
 {
+
     public int Number1{get;set;}
+
     public int Number2{get;set;}
+
     public int Number3{get;set;}
+
     public int Number4{get;set;}
+
     public int Number5{get;set;}
+
     public int Number6{get;set;}
+
     public int Number7{get;set;}
+
     public int Number8{get;set;}
+
     public int Number9{get;set;}
+
     public int Number10{get;set;}
 }
