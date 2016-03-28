dc8ed2f0d78b729f1c0f1f9cdce080c6592911f8
ChuckLu <244657538@qq.com>
2016-03-28 10:39:14 +0800

    添加SimpleT4Template

---
+++ ConsoleApp/SimpleT4Template.cs
@@ -0,0 +1,11 @@
+﻿ 
+// This is the output code from your template
+// you only get syntax-highlighting here - not intellisense
+namespace MyNameSpace{
+  class MyGeneratedClass{
+     static void main (string[] args){
+       System.Console.WriteLine("Hello, the time is now: 2016/3/28 10:38:31");
+     }
+  }
+}
+ 
