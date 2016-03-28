38db74caf3c4f1fcff6c081c043b5b5fd60e604e
ChuckLu <244657538@qq.com>
2016-03-28 11:12:53 +0800

    通过TextTemplate生成一个类，类中有10个int属性

--- ConsoleApp/TextTemplate.cs
+++ ConsoleApp/TextTemplate.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+public class Chuck
+{
+    public int Number1{get;set;}
+    public int Number2{get;set;}
+    public int Number3{get;set;}
+    public int Number4{get;set;}
+    public int Number5{get;set;}
+    public int Number6{get;set;}
+    public int Number7{get;set;}
+    public int Number8{get;set;}
+    public int Number9{get;set;}
+    public int Number10{get;set;}
+}
